b1400173b6f8592c6c0c5f68af704a67fec6899d
UZabauski <44897654+UZabauski@users.noreply.github.com>
2018-11-18 12:52:34 +0300

    homework4.1
    Thank you for review. All classes and methods have a meaningful names.

--- task.py
+++ task.py
@@ -4,7 +4,7 @@ import psutil
 from time import sleep
 
 
-class Class1(object):
+class Config_file(object):
     def __init__(self, myfile):
         self = open(myfile, 'w')
         self.write('interval = 5')
@@ -16,49 +16,49 @@ class Class1(object):
         self.close()
 
 
-c1 = Class1("config.py")
+c1 = Config_file("config.py")
 import config  # noqa
 
 
-class Class2(object):
-    def status1(self):
+class Psutil_stat(object):
+    def cpu(self):
         return str(psutil.cpu_percent(interval=1)) + '%'
 
-    def status2(self):
+    def vma(self):
         return str(psutil.virtual_memory().available / 1048576) + 'Mb'
 
-    def status3(self):
+    def vmu(self):
         return str(psutil.virtual_memory().used / 1048576) + 'Mb'
 
-    def status4(self):
+    def disk_io_c(self):
         return str(psutil.disk_io_counters().write_time) + 'ms'
 
-    def status5(self):
+    def net_io_c(self):
         return str(psutil.net_io_counters().bytes_sent) + 'b'
 
 
-class Class3(object):
-    def status(self):
+class Statistic_dict(object):
+    def dict_st(self):
         get_info = {
-            '1': c2.status1(),
-            '2': c2.status2(),
-            '3': c2.status3(),
-            '4': c2.status4(),
-            '5': c2.status5()}
+            '1': c2.cpu(),
+            '2': c2.vma(),
+            '3': c2.vmu(),
+            '4': c2.disk_io_c(),
+            '5': c2.net_io_c()}
         return get_info
 
 
-class Class4(object):
+class Output_to_file(object):
     def wrt(self):
         if config.output_type == "text":
             counter = 1
             while True:
-                get_status = c3.status()
-                get_1 = c3.status().get('1')
-                get_2 = c3.status().get('2')
-                get_3 = c3.status().get('3')
-                get_4 = c3.status().get('4')
-                get_5 = c3.status().get('5')
+                get_status = c3.dict_st()
+                get_1 = c3.dict_st().get('1')
+                get_2 = c3.dict_st().get('2')
+                get_3 = c3.dict_st().get('3')
+                get_4 = c3.dict_st().get('4')
+                get_5 = c3.dict_st().get('5')
                 get_6 = c5.my_time()
                 out_file = open(config.file_name, "a")
                 str0 = 'SNAPSHOT ' + str(counter) + ' : ' + str(get_6)
@@ -84,7 +84,7 @@ class Class4(object):
             while True:
                 get_status["Snapshot"] = "SNAPSHOT" + str(counter)
                 get_status["timestamp"] = str(c5.my_time())
-                get_status["status"] = c3.status()
+                get_status["status"] = c3.dict_st()
                 output_file = open(config.file_name, "a")
                 output_file.write(json.dumps(get_status) + "\n\n")
                 output_file.close()
@@ -94,13 +94,13 @@ class Class4(object):
                 counter += 1
 
 
-class Class5(object):
+class Sleep_interval(object):
     def my_time(self):
         return str(datetime.datetime.now().replace(microsecond=0))
 
 
-c2 = Class2()
-c3 = Class3()
-c4 = Class4()
-c5 = Class5()
+c2 = Psutil_stat()
+c3 = Statistic_dict()
+c4 = Output_to_file()
+c5 = Sleep_interval()
 c4.wrt()
